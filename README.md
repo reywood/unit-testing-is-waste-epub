8ac63a00afe0ee4823dcaf6ac7c39b090ade290e
Sean Dwyer <sean@rev.ai>
2023-07-31 16:36:20 -0700

    Add Segue essay

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-An epub version of James O Coplien's essay entitled Why Most Unit Testing is Waste
+Epub versions of James O Coplien's essays entitled Why Most Unit Testing is Waste and Segue
--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 DIR=$(realpath "$(dirname "$0")")
 
-cd "$DIR"/src || exit
+cd "$DIR/Why Most Unit Testing is Waste" || exit
 
-zip -rX "$DIR"/'Why Most Unit Testing is Waste.epub' \
+zip -rX "$DIR/Why Most Unit Testing is Waste.epub" \
     mimetype \
     META-INF \
     content.opf \
@@ -14,4 +14,19 @@ zip -rX "$DIR"/'Why Most Unit Testing is Waste.epub' \
     page_styles.css \
     stylesheet.css \
     titlepage.xhtml \
+    toc.ncx
+
+cd "$DIR/Segue" || exit
+
+zip -rX "$DIR/Segue.epub" \
+    mimetype \
+    META-INF \
+    content.opf \
+    index-1_1.jpg \
+    crc-card.png \
+    venn-diagram.png \
+    index_split_000.html \
+    index_split_001.html \
+    page_styles.css \
+    stylesheet.css \
     toc.ncx
